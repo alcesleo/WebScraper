87122376700096a6633fde557d9d267271f576a0
alcesleo <lagginglion@gmail.com>
2013-11-18 14:57:07 +0100

    Routing / to locallyproduced

--- WebScraper/urls.py
+++ WebScraper/urls.py
@@ -5,8 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'WebScraper.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'locallyproduced.views.show', name='show'),
     url(r'^admin/', include(admin.site.urls)),
 )
