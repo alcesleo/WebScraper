e18bbb6c77200dc6922dbab27f6bb33b861a4583
alcesleo <lagginglion@gmail.com>
2013-11-18 15:01:05 +0100

    Renaming project to lower case letters

--- webscraper/settings.py
+++ webscraper/settings.py
@@ -1,5 +1,5 @@
 """
-Django settings for WebScraper project.
+Django settings for webscraper project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/topics/settings/
@@ -48,9 +48,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'WebScraper.urls'
+ROOT_URLCONF = 'webscraper.urls'
 
-WSGI_APPLICATION = 'WebScraper.wsgi.application'
+WSGI_APPLICATION = 'webscraper.wsgi.application'
 
 
 # Database
