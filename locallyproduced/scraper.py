ec73dfc0aba8bcc84085b94e0c1c1315adf02d97
alcesleo <lagginglion@gmail.com>
2013-11-18 15:55:47 +0100

    Object oriented scraper

--- locallyproduced/scraper.py
+++ locallyproduced/scraper.py
@@ -1,20 +1,39 @@
 import requests
-import beautifulsoup
+from bs4 import BeautifulSoup
 
-# Define vars
-base_url = 'http://vhost3.lnu.se:20080/~1dv449/scrape/'
-login_url = base_url + 'check.php'
-product_url = base_url + 'secure/producenter.php'
+class Scraper(object):
 
-login_data = {
-    'username': 'admin',
-    'password': 'admin'
-}
+    # Define vars
+    base_url = 'http://vhost3.lnu.se:20080/~1dv449/scrape/'
+    login_url = base_url + 'check.php'
+    product_url = base_url + 'secure/producenter.php'
 
-# Log in
-s = requests.session()
-s.post(login_url, data=login_data)
+    login_data = {
+        'username': 'admin',
+        'password': 'admin'
+    }
 
-# Scrape
-r = s.get(product_url)
-print(r.text)
+    def get_logged_in_session(self):
+        """Returns requests session-object"""
+
+        # Log in
+        s = requests.session()
+        s.post(self.login_url, data=self.login_data)
+        return s
+
+    def scrape(self):
+
+        s = self.get_logged_in_session()
+
+        # Scrape
+        response = s.get(self.product_url)
+        soup = BeautifulSoup(response.text)
+        soup.encode('utf-8')
+        links = ''
+        for link in soup.find_all('a'):
+            print(link)
+
+
+# Test
+s = Scraper()
+s.scrape()
