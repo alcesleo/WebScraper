79eab8515be189742284f850cdb0046ed550ef5a
alcesleo <lagginglion@gmail.com>
2013-11-20 09:27:35 +0100

    Scrape on each request for now

--- locallyproduced/views.py
+++ locallyproduced/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from locallyproduced.models import Producer
+from locallyproduced.scraper import Scraper
 
-# Create your views here.
+# create your views here.
 def show(request):
+
+    # update the results
+    s = Scraper()
+    s.scrape()
+
+    # render the contents of db
     return render(request,
            'locallyproduced/producers.html',
            {'producers': Producer.objects.all()})
