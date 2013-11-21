df8916db0717df718bc6cc49e25fd3122a56a8b1
alcesleo <lagginglion@gmail.com>
2013-11-21 11:23:31 +0100

    Removed comment

--- webscraper/urls.py
+++ webscraper/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
     url(r'^$', 'locallyproduced.views.show', name='show'),
     url(r'^scrape$', 'locallyproduced.views.scrape', name='scrape'),
     url(r'^admin/', include(admin.site.urls)),
